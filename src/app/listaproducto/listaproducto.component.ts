02fc6b439c691175de0c42c88ae7cf3c299e6013
Carrl2023 <aniloracrom@gmail.com>
2025-03-24 20:48:50 -0500

    Se agregaran mas cambios

---
+++ src/app/listaproducto/listaproducto.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listaproducto',
+  imports: [],
+  templateUrl: './listaproducto.component.html',
+  styleUrl: './listaproducto.component.css'
+})
+export class ListaproductoComponent {
+
+}
